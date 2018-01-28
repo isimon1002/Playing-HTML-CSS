3a5c6d4beabdda4b54a86079515c3269d22319dd
Isaac Simon <ijsimon@sbcglobal.net>
2018-01-27 19:40:33 -0600

    User can change color of each collumn independantly

---
+++ index.js
@@ -0,0 +1,14 @@
+var changeColor = function(){
+  var sel1 = document.getElementById('background').value;
+  document.getElementById('collumn1').style.backgroundColor = sel1;
+}
+
+var changeColor2 = function(){
+  var sel1 = document.getElementById('background2').value;
+  document.getElementById('collumn2').style.backgroundColor = sel1;
+}
+
+var changeColor3 = function(){
+  var sel1 = document.getElementById('background3').value;
+  document.getElementById('collumn3').style.backgroundColor = sel1;
+}
